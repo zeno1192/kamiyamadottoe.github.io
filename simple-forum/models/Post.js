2e1b970e6c634caef5d28df5866b51bdcd0c5251
zeno1192 <165988729+zeno1192@users.noreply.github.com>
2024-10-22 08:35:57 +0900

    Delete simple-forum directory

--- simple-forum/models/Post.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const postSchema = new mongoose.Schema({
-    content: { type: String, required: true },   // 投稿内容
-    createdAt: { type: Date, default: Date.now } // 作成日時
-});
-
-module.exports = mongoose.model('Post', postSchema);
